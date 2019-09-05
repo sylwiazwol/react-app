af4ac342ab2cf82675bbeaefe2a06d465872886b
sylwiazwol <48494412+sylwiazwol@users.noreply.github.com>
2019-09-05 13:44:42 +0200

    Updates

---
+++ precache-manifest.176ce67213ca3f11d3401b291bc1e2d2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "d9d36b65a132d26aed884ad58e4719eb",
+    "url": "/react-app/static/media/header3.d9d36b65.jpg"
+  },
+  {
+    "revision": "33eca35fceea8311d67358a6629fa9d9",
+    "url": "/react-app/static/media/header2.33eca35f.jpg"
+  },
+  {
+    "revision": "70e6670ee746fc59f414fde4fcce93af",
+    "url": "/react-app/static/media/header1.70e6670e.jpg"
+  },
+  {
+    "revision": "8ce05037bfd6015f067a",
+    "url": "/react-app/static/js/runtime~main.8ce05037.js"
+  },
+  {
+    "revision": "7815ceec9a06ccb3dbbb",
+    "url": "/react-app/static/js/main.7815ceec.chunk.js"
+  },
+  {
+    "revision": "bdc9a5b0dec48b28481c",
+    "url": "/react-app/static/js/2.bdc9a5b0.chunk.js"
+  },
+  {
+    "revision": "7815ceec9a06ccb3dbbb",
+    "url": "/react-app/static/css/main.08736969.chunk.css"
+  },
+  {
+    "revision": "ce0c8f9ff8617bac963e172de10658a6",
+    "url": "/react-app/index.html"
+  }
+];
